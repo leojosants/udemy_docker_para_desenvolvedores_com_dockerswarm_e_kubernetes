ba84d59cb9ccf1ce688a60e69b59a8a9873c17d3
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-23 18:41:37 -0300

    64. Creating the project to work with Volumes

---
+++ 02_introduzindo_volumes_aos_nossos_containers/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mensagens</title>
+</head>
+
+<body>
+    <h1>Escreva sua mensagem</h1>
+    <form action="process.php" method="post">
+        <input type="text" name="message" id="message" placeholder="Digite a mensagem">
+        <input type="submit" value="Enviar mensagem">
+    </form>
+</body>
+
+</html>
