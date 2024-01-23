90d26c371cb00988aa68c37f0a2f4fdc9d819bad
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-23 14:22:54 -0300

    51. Copying files from the container

---
+++ 01_imagens_e_containers/copia/app.js
@@ -0,0 +1,13 @@
+const express = require('express'); // import
+const app = express(); // inicializando
+const port = 3000;
+const link = `https://localhost:${port}`
+
+app.get('/', (req, res) => {
+    res.send('Olá, minha imagem!!!');
+});
+
+app.listen(port, () => {
+    console.log(`Executando na porta ${port}!`);
+    console.log(`CTRL + click ${link}`);
+})
