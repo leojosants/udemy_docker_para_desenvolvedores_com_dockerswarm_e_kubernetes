36727b9f262e7d03995c1f8fa4738cef729d6eba
Leonardo Santos <leonardojosantos@gmail.com>
2024-02-01 18:32:27 -0300

    116. Build images in Compose

---
+++ 05_gerenciando_multiplos_containers_com_docker_compose/05_build_compose/flask/app.py
@@ -0,0 +1,48 @@
+from flask import Flask
+
+from flask import request, json, jsonify
+
+import requests
+
+from flask_mysqldb import MySQL
+
+# iniciar
+app = Flask(__name__)
+
+# exibicao de mensagens no terminal
+app.config["DEBUG"] = True
+
+# configuracao MySQL
+app.config['MYSQL_HOST'] = 'db'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'l1j1o1s1'
+app.config['MYSQL_DB'] = 'flaskdocker'
+
+mysql = MySQL(app)
+
+# rota para receber dados da api externa
+
+
+@app.route("/", methods=["GET"])
+def index():
+    data = requests.get('https://randomuser.me/api')
+    return data.json()
+
+# rota para insercao de dados
+
+
+@app.route("/inserthost", methods=["POST"])
+def inserthost():
+    data = requests.get('https://randomuser.me/api').json()
+    username = data['results'][0]['name']['first']
+    cur = mysql.connect.cursor()
+    # cur = mysql.connection.cursor()
+    cur.execute("""INSERT INTO users(name) VALUES(%s)""", (username,))
+    mysql.connect.commit()
+    # mysql.connection.commit()
+    cur.close()
+    return "usuario inserido: " + username
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port="5000")  # type: ignore
